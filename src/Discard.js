4a481b07a409afdff25be64de257372f1e47fd08
unknown <andrew.ning.cs@gmail.com>
2020-06-10 16:18:45 -0400

    fixed discard pile indexing

--- src/Discard.js
+++ src/Discard.js
@@ -62,8 +62,8 @@ class Discard extends React.Component {
 
     render(){
         let rows = []
-        for (let i = 1; i < Math.ceil(this.state.discard.length/maxTilesPerRow); i++){
-            rows.push(<div key={i}>{this.renderTileRow(i * maxTilesPerRow, maxTilesPerRow)}</div>)
+        for (let i = 1; i < this.state.discard.length; i+=maxTilesPerRow){
+            rows.push(<div key={i}>{this.renderTileRow(i, maxTilesPerRow)}</div>)
         }
         let lastDiscard = "";
         if (this.state.discard.length > 0) {
